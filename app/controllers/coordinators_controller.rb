97aacc5d5e62540bd6a514e249e667ef4ec7879d
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-23 21:10:21 +0530

    Added Flash message

--- app/controllers/coordinators_controller.rb
+++ app/controllers/coordinators_controller.rb
@@ -1,5 +1,6 @@
 class CoordinatorsController < ApplicationController
   def dashboard
+    flash[:success] = "Welcome to the dashboard!!"
     @current_coordinator = Coordinator.find(current_coordinator.id)    # First finds the current logged in coordinator
   end
 end
