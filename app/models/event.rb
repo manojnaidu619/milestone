5b0113f2d41108919037e053c185c5006cbc48b2
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-30 22:03:52 +0530

    Need to make card as link

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   has_many_attached :image                    # Event has many images
   belongs_to :coordinator                    # Each event belongs to a coordinator
-  validates :title, presence: true, length: { minimum: 15 }
+  validates :title, presence: true, length: { minimum: 15, maximum: 45 }
   validates :description, presence:true, length: { minimum: 30 }
 
 end
