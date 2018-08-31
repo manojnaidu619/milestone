afdff89545e186634987f66efbac7e5e064090fe
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-31 22:59:47 +0530

    Formatted Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,2 @@
 module ApplicationHelper
-
-  def markdown(text) 
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                                       no_intra_emphasis: true,
-                                       fenced_code_blocks: true,
-                                       disable_indented_code_blocks: true,
-                                       autolink: true,
-                                       tables: true,
-                                       underline: true,
-                                       highlight: true
-                                      )
-     return markdown.render(text).html_safe
-  end
-
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
 module EventsHelper
+  
+  def markdown(text)                                                  # parameter as 'text' is imporatant
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                       no_intra_emphasis: true,
+                                       fenced_code_blocks: true,
+                                       disable_indented_code_blocks: true,
+                                       autolink: true,
+                                       tables: true,
+                                       underline: true,
+                                       highlight: true
+                                      )
+     return markdown.render(text).html_safe                           # Returning the Markdown format
+  end
+
 end
