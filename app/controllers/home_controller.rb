55d5eb4208c52e79d61ef73363bbc78ace62c410
Manoj Naidu <manojnaidu619@gmail.com>
2018-09-02 20:32:17 +0530

    Eager Loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @events = Event.all.order(created_at: :desc).includes(:coordinator)        # Implemented Eager Loading, link: https://www.youtube.com/watch?v=s2EPVMqOsTQ
+    @events = Event.all.order(created_at: :desc).includes(:coordinator => :events)        # Implemented Eager Loading, link: https://www.youtube.com/watch?v=s2EPVMqOsTQ
     if coordinator_signed_in?
       redirect_to dashboard_path
     end
