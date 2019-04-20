781c68c00db1d5bbd06972149913c02775a56cf8
Manoj Naidu <manojnaidu619@gmail.com>
2019-04-20 13:23:32 +0530

    crud model edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,12 @@
 class Event < ApplicationRecord
+
+  extend FriendlyId                      # For friendly-id
+  friendly_id :title, use: :slugged
+
   has_many_attached :image                    # Event has many images
   belongs_to :coordinator                    # Each event belongs to a coordinator
   validates :title, presence: true, length: { minimum: 15, maximum: 45 }
   validates :description, presence:true, length: { minimum: 30 }
-
-  extend FriendlyId                      # For friendly-id
-  friendly_id :title, use: [:slugged]
 
   def should_generate_new_friendly_id?     # Method given by friendly_id
   title_changed?                           # Create new slug if the title gets updated
